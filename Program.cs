4652cc3ae97ee56d01c3b956580b39819d35b5a1
gals <developergals@gmail.com>
2019-08-09 19:20:03 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_PermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] A = {1,3,2,4};
+
+            
+
+
+            long n = A.Length + 1;
+
+            var sumOfAllElements = (n * (1 + n)) / 2;
+            var missingElement = sumOfAllElements - A.Select(x => (long)x).Sum();
+            //return (int)missingElement ; 
+
+            Console.WriteLine((int)missingElement);
+            Console.ReadKey();
+
+
+        }
+    }
+}
